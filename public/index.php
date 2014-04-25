5479b7842ef23d18787e2fa08fdcf51f96aeb177
Juan Torres <juanin.torres@gmail.com>
2014-04-25 19:08:01 -0300

    Un lapsus

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ $mustache = new Mustache_Engine(array(
     'cache_file_mode' => 0666, // Please, configure your umask instead of doing this :)
     'cache_lambda_templates' => true,
     'loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../views'),
-    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/views/partials'),
+    'partials_loader' => new Mustache_Loader_FilesystemLoader(dirname(__FILE__).'/../views/partials'),
     'helpers' => array('i18n' => function($text) {
         // do something translatey here...
     }),
