c3b33159e4d4f5b6af5774ba14fd9376c4e86146
Juan Torres <juanin.torres@gmail.com>
2014-04-25 18:39:47 -0300

    Hola Mundo !!!

--- public/index.php
+++ public/index.php
@@ -3,4 +3,6 @@
 require_once '../vendor/autoload.php';
 Mustache_Autoloader::register();
 
+$m = new Mustache_Engine;
+echo $m->render('Hello, {{planet}}!', array('planet' => 'World')); // "Hello, World!"
 ?>
