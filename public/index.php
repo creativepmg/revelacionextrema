10a9ec83d865a062db87a446a954f1c0e12c8100
Juan Torres <juanin.torres@gmail.com>
2014-04-25 18:23:54 -0300

    Comenzando con mustache

--- public/index.php
+++ public/index.php
@@ -1,20 +1,6 @@
 <?php
-require_once('../vendor/autoload.php');
 
+require_once '../vendor/autoload.php';
+Mustache_Autoloader::register();
 
-$smarty = new Smarty();
-
-$smarty->setTemplateDir('../templates/');
-$smarty->setCompileDir('../templates_c/');
-$smarty->setConfigDir('../configs/');
-$smarty->setCacheDir('../cache/');
-
-$smarty->assign('name','Juan');
-
-//** un-comment the following line to show the debug console
-$smarty->debugging = true;
-
-$smarty->display('test.tpl');
-
-// echo SMARTY_DIR;
 ?>
